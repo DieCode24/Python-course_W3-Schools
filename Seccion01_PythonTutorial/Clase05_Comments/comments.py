f057d6458c2556f4783edd0fa51132bff7eb8e9b
DieCode24 <diego.giraldo.0424@gmail.com>
2024-05-15 23:42:32 -0500

    Created comments.py file, with comments

---
+++ Seccion01_PythonTutorial/Clase05_Comments/comments.py
@@ -0,0 +1,24 @@
+# Importación del módulo sys, que proporciona acceso a variables y funciones mantenidas o controladas por el intérprete de Python.
+import sys
+
+# Este es un comentario.
+
+# Impresión del string "Hello, World!".
+print("Hello, World!")
+
+"""
+Este es un comentario
+escrito en
+más de una línea
+"""
+
+# Impresión del string "\n\nComentario extendido".
+print("\n\nComentario extendido")
+
+'''
+Salida del programa.
+
+La función exit() termina la ejecución del programa de Python.
+Aunque en este punto no se realiza ninguna acción adicional, el uso de exit() asegura una terminación limpia del programa.
+'''
+exit()
