0906d3da0bba974bc3e75a62017d193126d61151
DieCode24 <diego.giraldo.0424@gmail.com>
2024-05-15 23:12:56 -0500

    Added comments getStarted.py file

--- Seccion01_PythonTutorial/Clase03_GetStarted/getStarted.py
+++ Seccion01_PythonTutorial/Clase03_GetStarted/getStarted.py
@@ -1,5 +1,13 @@
+# Importación del módulo sys, que proporciona acceso a variables y funciones mantenidas o controladas por el intérprete de Python.
 import sys
 
+# Impresión de la versión de Python instalada en el sistema utilizando la función version del módulo sys.
 print(sys.version)
 
+'''
+Salida del programa.
+
+La función exit() termina la ejecución del programa de Python.
+Aunque en este punto no se realiza ninguna acción adicional, el uso de exit() asegura una terminación limpia del programa.
+'''
 exit()
