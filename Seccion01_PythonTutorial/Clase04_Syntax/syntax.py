c63baf5706947ce0524ae7d4011494d58cf113f8
DieCode24 <diego.giraldo.0424@gmail.com>
2024-05-15 23:24:33 -0500

    Added comments in syntax.py file

--- Seccion01_PythonTutorial/Clase04_Syntax/syntax.py
+++ Seccion01_PythonTutorial/Clase04_Syntax/syntax.py
@@ -1,9 +1,15 @@
+# Importación del módulo sys, que proporciona acceso a variables y funciones mantenidas o controladas por el intérprete de Python.
 import sys
 
+# Comprobación condicional: Si 5 es mayor que 2.
 if 5 > 2:
+    # Si la condición es verdadera, se imprime el mensaje "Five is greater than two!".
     print("Five is greater than two!")
     
+# Asignación del valor 5 a la variable x.
 x = 5
-y = "Hello, World!"
+# Asignación del string "Hello, World!" a la variable y.
+y = "\n\nHello, World!"
 
-print(y + "\nNumber => " + x)
+# Impresión de la concatenación del string "Hello, World!" con el string "\nNumber => " y el valor de la variable x convertido a string.
+print(y + "\nNumber => " + str(x))
