9318c66ed250830dad07a28adcdecab0119535e1
DieCode24 <diego.giraldo.0424@gmail.com>
2024-05-16 23:09:02 -0500

    added comments variables.py file

--- Seccion01_PythonTutorial/Clase06_Variables/variables.py
+++ Seccion01_PythonTutorial/Clase06_Variables/variables.py
@@ -1,30 +1,33 @@
-x = 5
-y = "John"
-print(x)
-print(y)
+# Asignación inicial de variables
+x = 5        # x es de tipo int (entero) y tiene el valor 5
+y = "John"   # y es de tipo str (cadena) y tiene el valor "John"
 
+# Imprimir los valores de las variables
+print(x)     # Esto imprimirá: 5
+print(y)     # Esto imprimirá: John
 
-x = 4       # x is of type int
-x = "Sally" # x is now of type str
-print(x)
+# Reasignación de la variable x
+x = 4        # x es de tipo int (entero) y tiene el valor 4
+x = "Sally"  # x ahora es de tipo str (cadena) y tiene el valor "Sally"
+print(x)     # Esto imprimirá: Sally
 
+# Conversión de tipos de datos
+x = str(3)    # x será '3' (cadena)
+y = int(3)    # y será 3 (entero)
+z = float(3)  # z será 3.0 (número de punto flotante)
 
-x = str(3)    # x will be '3'
-y = int(3)    # y will be 3
-z = float(3)  # z will be 3.0
+# Uso de la función type() para verificar los tipos de datos
+x = 5         # x es de tipo int (entero) y tiene el valor 5
+y = "John"    # y es de tipo str (cadena) y tiene el valor "John"
+print(type(x)) # Esto imprimirá: <class 'int'>
+print(type(y)) # Esto imprimirá: <class 'str'>
 
+# Asignación de cadenas usando comillas simples y dobles
+x = "John"    # Asignación de cadena usando comillas dobles
+# es lo mismo que
+x = 'John'    # Asignación de cadena usando comillas simples
 
-x = 5
-y = "John"
-print(type(x))
-print(type(y))
-
-
-x = "John"
-# is the same as
-x = 'John'
-
-
-a = 4
-A = "Sally"
-#A will not overwrite a
+# Sensibilidad a mayúsculas en nombres de variables
+a = 4         # a es de tipo int (entero) y tiene el valor 4
+A = "Sally"   # A es de tipo str (cadena) y tiene el valor "Sally"
+# A no sobrescribirá a
